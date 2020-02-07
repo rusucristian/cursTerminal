51d9890d5aa47e74c0cdafe1a940359a1af90c09
Cristian <rusu.cristian@mail.ru>
2020-02-07 20:06:56 +0200

    add method to get current day

--- src/main/java/com/rusu/Main.java
+++ src/main/java/com/rusu/Main.java
@@ -6,9 +6,17 @@ import org.jsoup.nodes.Document;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Document page = getPage();
+
+        getDate();
+
+
 
     }
 
@@ -16,5 +24,11 @@ public class Main {
         String url = "https://bnm.md/";
         Document pageDocument = Jsoup.parse(new URL(url), 10000);
         return pageDocument;
+    }
+
+    private static void getDate(){
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Date date = new Date();
+        System.out.printf("%13s\n", dateFormat.format(date));
     }
 }
