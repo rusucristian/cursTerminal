7f12a23995f54f8d7c18365c40553802d0a70a8a
Cristian <rusu.cristian@mail.ru>
2020-02-08 11:47:36 +0200

    minor changes

--- src/main/java/com/rusu/Main.java
+++ src/main/java/com/rusu/Main.java
@@ -1,22 +1,11 @@
 package com.rusu;
 
-import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         com.rusu.Elements elements = new com.rusu.Elements();
-
-        Document page = elements.getPage();
 
         elements.getDate();
 
